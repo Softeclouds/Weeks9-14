8e92216e9d65900bffd28d204f5f03c05b51d9f4
SofteClouds <kaimck05@gmail.com>
2025-03-20 11:00:56 -0400

    Completed coding gym challenge 2

--- Assets/CodingGymWeek10/Scripts/growCoroutine.cs
+++ Assets/CodingGymWeek10/Scripts/growCoroutine.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class growCoroutine : MonoBehaviour
 {
     public Vector3 targetScale = new Vector3(2f, 2f, 2f);
     public float duration = 5f;
     public AnimationCurve curve;
+    public Button button;
 
     void Start()
     {
-        StartCoroutine(grow(targetScale, duration));
+        
     }
 
     public IEnumerator grow(Vector3 targetScale, float duration)
     {
+        Debug.Log("currently attacking");
+        button.interactable = false;
         Vector3 scale = transform.localScale;
         float t = 0f;
 
@@ -26,6 +30,18 @@ public class growCoroutine : MonoBehaviour
             yield return null;
         }
 
+        if(t >= duration)
+        {
+            Debug.Log("Attact is over");
+            button.interactable = true;
+        }
+
         transform.localScale = targetScale;
+    }
+
+
+    public void onAttack()
+    {
+        StartCoroutine(grow(targetScale, duration));
     }
 }
