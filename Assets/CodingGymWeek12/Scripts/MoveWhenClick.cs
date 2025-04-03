65ce21ddf3924e045369e3eda5e6032c4563cb44
SofteClouds <kaimck05@gmail.com>
2025-04-03 10:55:56 -0400

    Completed challenge 3 Dont walk on grass

--- Assets/CodingGymWeek12/Scripts/MoveWhenClick.cs
+++ Assets/CodingGymWeek12/Scripts/MoveWhenClick.cs
@@ -11,25 +11,39 @@ public class MoveWhenClick : MonoBehaviour
 
     public Tilemap stoneTilemap;
 
+    public AudioSource audioSource;
+    public AudioClip[] clips;
+
+    private SpriteRenderer sr;
+    private Animator animator;
+
+    private bool isOnStone = false;
+
     // Start is called before the first frame update
     void Start()
     {
         targetPos = transform.position;
+
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0)) // if mouse is being pressed
         {
-            Debug.Log("CLICK");
+            // Debug.Log("CLICK");
             SetTargetPos(Input.mousePosition);
         }
 
         if (isMoving)
         {
-            Debug.Log("MOVE");
+            // Debug.Log("MOVE");
             Move();
+            CheckTile();
         }
     }
 
@@ -52,12 +66,45 @@ public class MoveWhenClick : MonoBehaviour
         Vector2 pos = transform.position;
         Vector2 direction = (targetPos - pos).normalized;
 
+        sr.flipX = direction.x < 0;
+        animator.SetFloat("Movement", Mathf.Abs(direction.x));
+
         transform.position = new Vector2(pos.x + direction.x * speed * Time.deltaTime, pos.y + direction.y * speed * Time.deltaTime); // update position with direction and speed
 
 
         if (Vector2.Distance(transform.position, targetPos) < 0.1f)
         {
             isMoving = false; // stop moving if target is reached
+            animator.SetFloat("Movement", 0);
         }
     }
+
+    void CheckTile()
+    {
+        Vector2 playerPos = transform.position;
+
+        Vector3 cellWorldPos = stoneTilemap.WorldToCell(playerPos); // convert players pos to tilemap world position
+        Vector3 cellCenterWorldPosition = stoneTilemap.GetCellCenterWorld(stoneTilemap.WorldToCell(playerPos)); // calculate center of the tile
+
+        if (stoneTilemap.HasTile(stoneTilemap.WorldToCell(playerPos)) && Vector3.Distance(playerPos, cellCenterWorldPosition) < 0.5f) // check if the player is on a stone tile
+        {
+            Debug.Log("on stone!");
+            isOnStone = true;
+
+        }
+        else
+        {
+            isOnStone = false;
+        }
+    }
+
+        public void playFootStep()
+        {
+            if (isOnStone) // only plays audio is the player is on stone
+            {
+                audioSource.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+                // syncs audio with animation
+            }
+        }
+    
 }
